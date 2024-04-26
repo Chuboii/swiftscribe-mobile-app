0cb2e00dec366018d470d8faaacbb278c5187c9c
Chuboii <favoronyechere@gmail.com>
2024-04-26 11:55:26 +0100

    commit

--- navigations/stack navigation/StackNavigtion.tsx
+++ navigations/stack navigation/StackNavigtion.tsx
@@ -1,19 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import SignUpScreen from '../../screens/auth/sign up screen/SignUpScreen'
 import TabNavigation from '../tab navigation/TabNavigation'
 import AccountRecoveryScreen from '../../screens/auth/account recovery screen/AccountRecoveryScreen'
+import theme from '../../utils/theme/theme/theme'
 
 
 const Stack = createNativeStackNavigator()
 
+const uiTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: "red"
+    }
+}
 
 const StackNavigtion = () => {
 
   return (
-      <NavigationContainer>
+      <NavigationContainer theme={uiTheme}>
           <Stack.Navigator screenOptions={{headerShown:false}}>
               <Stack.Screen name="sign up" component={SignUpScreen} />
               <Stack.Screen name="account recovery" component={AccountRecoveryScreen} />
