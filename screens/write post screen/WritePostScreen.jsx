02e9d11b032b872dccb386dd993b4b3e7a359da4
Chuboii <favoronyechere@gmail.com>
2024-05-03 17:14:29 +0100

    commit

---
+++ screens/write post screen/WritePostScreen.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useLayoutEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import QuillEditor, { QuillToolbar } from "react-native-cn-quill";
+import theme from "../../utils/theme/theme/theme";
+import Button from "../../components/button template/Button";
+import { styles } from "./WritePostScreen.style";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function App() {
+  const _editor = React.createRef();
+
+  const toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "video", "formula"],
+    ["image"],
+    [({ header: 1 }, { header: 2 })], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ];
+  // const toolbarOptions = {
+  //   handlers: {
+  //     // handlers object will be merged with default handlers object
+  //     link: function (value) {
+  //       if (value) {
+  //         const href = prompt("Enter the URL");
+  //         this.quill.format("link", href);
+  //       } else {
+  //         this.quill.format("link", false);
+  //       }
+  //     },
+  //   },
+  // };
+
+  const a = () => {
+    _editor.current.dangerouslyPasteHTML(
+      0,
+      "<img src='https://plus.unsplash.com/premium_photo-1675884829570-83a41714113b?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>"
+    );
+  };
+  return (
+    <SafeAreaView style={styles.root}>
+      <View style={styles.wrapper}>
+        <Button expoIcon={<AntDesign name="close" style={styles.icon} />} />
+        <View style={styles.wrapper}>
+          <Button
+            helperFunction={a}
+            expoIcon={
+              <FontAwesome
+                name="image"
+                style={[styles.icon, { fontSize: 30 }]}
+              />
+            }
+          />
+          <Button
+            containerStyle={styles.btn}
+            text={"Preview"}
+            textStyle={styles.btnText}
+          />
+        </View>
+      </View>
+
+      <QuillEditor
+        loading="loading"
+        theme={{
+          background: theme().background,
+        }}
+        container={true} // Make sure to enable the wrapping container (also custom container)
+        ref={_editor}
+        style={styles.editor}
+      />
+      <View>
+        <QuillToolbar editor={_editor} options={toolbarOptions} theme="dark" />
+      </View>
+    </SafeAreaView>
+  );
+}
--- screens/write post screen/WritePostScreen.style.ts
+++ screens/write post screen/WritePostScreen.style.ts
@@ -1,5 +1,41 @@
-import {StyleSheet} from "react-native"
+import { Dimensions, StatusBar, StyleSheet } from "react-native";
+
+const screenHeight = Dimensions.get("window").height;
 
 export const styles = StyleSheet.create({
-  
-})
+  container: {},
+  btn: {
+    padding: 10,
+    paddingHorizontal: 30,
+    backgroundColor: "green",
+    borderRadius: 7,
+    marginLeft: 10,
+  },
+  btnText: { color: "white", fontFamily: "kanit-medium" },
+  title: {
+    fontWeight: "bold",
+    alignSelf: "center",
+    paddingVertical: 10,
+  },
+  root: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "white",
+  },
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  editor: {
+    flex: 1,
+    padding: 0,
+    minHeight: 500,
+    backgroundColor: "white",
+    paddingBottom: 150,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
