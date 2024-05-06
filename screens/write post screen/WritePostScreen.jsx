d3a878e1300f0cba1b16c752a0fbf4668b32293c
Chuboii <favoronyechere@gmail.com>
2024-05-06 03:20:21 +0100

    commit

--- screens/write post screen/WritePostScreen.jsx
+++ screens/write post screen/WritePostScreen.jsx
@@ -18,10 +18,10 @@ const handleHead = ({ tintColor }) => (
 const TempScreen = () => {
   const richText = React.useRef();
 
-  richText.current.insertImage(
-    "https://plus.unsplash.com/premium_photo-1675884829570-83a41714113b?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  );
-  console.log(richText.current);
+  // richText.current.insertImage(
+  //   "https://plus.unsplash.com/premium_photo-1675884829570-83a41714113b?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  // );
+  // console.log(richText.current);
   return (
     <SafeAreaView>
       <ScrollView>
