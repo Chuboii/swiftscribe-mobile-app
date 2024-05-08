c8ad011d71f64e911b63be31b82fadfb14683901
Chuboii <favoronyechere@gmail.com>
2024-05-08 09:51:08 +0100

    commit

--- components/drawer bottom template/DrawerBottom.tsx
+++ components/drawer bottom template/DrawerBottom.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import PostPreviewTemplate from '../post preview template/PostPreviewTemplate'
 
 const DrawerBottom = () => {
     return (
         <SafeAreaView>
-            <Text>DrawerBottom</Text>
+            <PostPreviewTemplate />
         </SafeAreaView>
     )
 }
